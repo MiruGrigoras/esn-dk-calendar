85ceafe3b82a345d97097acc6a782a6057295599
MiruGrigoras <miruna.grig@gmail.com>
2024-08-27 21:22:01 +0200

    feat: use global constants, add multiple calendars and remove unused code

--- src/components/MainCalendar.vue
+++ src/components/MainCalendar.vue
@@ -1,31 +1,18 @@
 <template>
   <div>
     <h1>Google Calendar Events</h1>
-    <button @click="fetchEvents">Fetch Events</button>
-    <ul v-if="events.length">
-      <li v-for="event in events" :key="event.id">
-        <strong>{{ event.summary }}</strong
-        ><br />
-        {{ formatDate(event.start.dateTime || event.start.date) }} -
-        {{ formatDate(event.end.dateTime || event.end.date) }}
-      </li>
-    </ul>
     <FullCalendar :options="calendarOptions" />
   </div>
 </template>
 
 <script>
-import {
-  initClient,
-  signIn,
-  getCalendarEvents,
-} from "../services/googleCalendar";
-
 import FullCalendar from "@fullcalendar/vue3";
 
 import dayGridPlugin from "@fullcalendar/daygrid";
 import googleCalendarPlugin from "@fullcalendar/google-calendar";
 import timeGridPlugin from "@fullcalendar/timegrid";
+
+import { API_KEY, CALENDAR_ID_1, CALENDAR_ID_2 } from "@/constants.js";
 
 export default {
   components: {
@@ -37,10 +24,20 @@ export default {
       calendarOptions: {
         plugins: [dayGridPlugin, googleCalendarPlugin, timeGridPlugin],
         initialView: "dayGridMonth",
-        events: {
-          googleCalendarId: "miruna.grig@gmail.com",
-          googleCalendarApiKey: "AIzaSyC6qB9xs-OgpYHy5L_CD9kiFK0OyMHj5eo",
-        },
+        eventSources: [
+          {
+            googleCalendarId: CALENDAR_ID_1,
+            googleCalendarApiKey: API_KEY,
+            color: "pink", // an option!
+            textColor: "black", // You can add custom class names to style events from different calendars
+          },
+          {
+            googleCalendarId: CALENDAR_ID_2,
+            googleCalendarApiKey: API_KEY,
+            color: "green", // an option!
+            textColor: "white",
+          },
+        ],
         headerToolbar: {
           left: "prev,next today",
           center: "title",
@@ -49,36 +46,5 @@ export default {
       },
     };
   },
-  methods: {
-    async fetchEvents() {
-      try {
-        await initClient();
-        await signIn();
-        const response = await getCalendarEvents();
-        this.events = response.result.items;
-      } catch (error) {
-        console.error("Error fetching events:", error);
-      }
-    },
-    formatDate(dateStr) {
-      const options = {
-        weekday: "short",
-        year: "numeric",
-        month: "short",
-        day: "numeric",
-      };
-      return new Date(dateStr).toLocaleDateString(undefined, options);
-    },
-  },
 };
 </script>
-
-<style scoped>
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  margin: 10px 0;
-}
-</style>
---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const CLIENT_ID =
+  "911289587188-ea6ba7m5ua4i6hqnot87k90q74ecoqes.apps.googleusercontent.com";
+export const API_KEY = "AIzaSyC6qB9xs-OgpYHy5L_CD9kiFK0OyMHj5eo";
+export const CALENDAR_ID_1 = "miruna.grig@gmail.com";
+export const CALENDAR_ID_2 =
+  "0204d4c2a1dca41e34c9982359bfd3885d3f2f14c522f77398acafed7088523d@group.calendar.google.com";
+export const DISCOVERY_DOCS = [
+  "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+];
+export const SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
